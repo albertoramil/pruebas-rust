fd95a3f929c11350201720fd606e2010c37bcde9
albertoramil <alberto.ramil@norvoz.es>
2022-09-20 10:14:34 +0200

    menu simplificado

--- src/main.rs
+++ src/main.rs
@@ -1,22 +1,16 @@
 pub mod rectangulotype;
 use std::fmt::format;
 
-use crate::rectangulotype::muestra;
 use crate::rectangulotype::calculate_length;
-use crate::rectangulotype::Rectangle;
+use crate::rectangulotype::muestra;
 use crate::rectangulotype::Coordenada;
+use crate::rectangulotype::Rectangle;
 pub mod triangulorectangulotype;
-
 
 use crate::triangulorectangulotype::Coordenadatri;
 use crate::triangulorectangulotype::Triangulo;
 
-
 pub mod rlib;
-
-
-
-
 
 fn main() {
     println!("mostrat {}", muestra());
@@ -43,7 +37,6 @@ fn main() {
     println!("Coordenada 2 {:?} print!", micoordenada2);
 
     //************************************* */
-
     let mirectangulo: Rectangle = Rectangle {
         p1: (micoordenada1),
         p2: (micoordenada2),
@@ -53,14 +46,12 @@ fn main() {
     println!("Perimetro {:?} print!", mirectangulo.perimeter());
 
     //************************************* */
-
     let micoordenada3 = Coordenada::origen();
     println!("Coordenada creada {:?} print!", micoordenada3);
     let micoordenada4 = Coordenada::nueva(2.0, 6.0);
     println!("Coordenada creada {:?} print!", micoordenada4);
 
     //************************************* */
-
     let mirectangulo2: Rectangle = Rectangle {
         p1: Coordenada::origen(),
         p2: Coordenada::nueva(5.0, 8.0),
@@ -69,38 +60,29 @@ fn main() {
     println!("Area {:?} print!", mirectangulo2.area());
     println!("Perimetro {:?} print!", mirectangulo2.perimeter());
 
-
     //********************** */
     let mitriangulo: Triangulo = Triangulo {
-      p1: Coordenadatri::origen(),
-      p2: Coordenadatri::nueva(2.0, 2.0),
-  };
-  println!("Triangulo {:?} print!", mitriangulo);
-  println!("Area {:?} print!", mitriangulo.area());
-  println!("Perimetro  triangulo{:?} print!", mitriangulo.perimeter());
+        p1: Coordenadatri::origen(),
+        p2: Coordenadatri::nueva(2.0, 2.0),
+    };
+    println!("Triangulo {:?} print!", mitriangulo);
+    println!("Area {:?} print!", mitriangulo.area());
+    println!("Perimetro  triangulo{:?} print!", mitriangulo.perimeter());
 
- 
+    let mut entrada = rlib::Command::new();
 
-let mut entrada = rlib::Command::new();
+    while entrada.accion != "salir" {
+        entrada = rlib::get_input();
+        if entrada.accion == "triangulo" {
+            println!("Triangulo {:?} print!", mitriangulo);
+            println!("Area {:?} print!", mitriangulo.area());
+            println!("Perimetro  triangulo{:?} print!", mitriangulo.perimeter());
+        }
 
-while entrada.accion != "salir" {
-  entrada = rlib::get_input();
-if  entrada.accion == "triangulo" {
-  println!("Triangulo {:?} print!", mitriangulo);
-  println!("Area {:?} print!", mitriangulo.area());
-  println!("Perimetro  triangulo{:?} print!", mitriangulo.perimeter());}
-
-
-
-if  entrada.accion == "rectangulo" {
-  println!("Rectangulo {:?} print!", mirectangulo2);
-  println!("Area {:?} print!", mirectangulo2.area());
-  println!("Perimetro {:?} print!", mirectangulo2.perimeter());}
- 
-
-
-}
-
-
-
+        if entrada.accion == "rectangulo" {
+            println!("Rectangulo {:?} print!", mirectangulo2);
+            println!("Area {:?} print!", mirectangulo2.area());
+            println!("Perimetro {:?} print!", mirectangulo2.perimeter());
+        }
+    }
 }
